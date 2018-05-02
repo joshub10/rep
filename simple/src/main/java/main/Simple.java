2a801e250e1b183043a8e3e9fdcd4f79aacda73d
Vitaliy <Vitaliy@DESKTOP-U8AB0SP>
2018-05-02 10:48:27 +0200

    ert

---
+++ simple/src/main/java/main/Simple.java
@@ -0,0 +1,24 @@
+package main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Simple{
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+	public static String main() {
+		System.out.println("------");
+		return "index";
+	}
+	@PostMapping("/")
+	public static String main2() {
+		return "index";
+	}
+}
